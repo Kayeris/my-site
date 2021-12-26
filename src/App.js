32e427a53a0ea54ba98338855c00e25ab4cceaca
Kayeris <shaiza@uoguelph.ca>
2021-12-26 14:17:29 -0500

    created base of site, routes, and added bg for landing page. also turned on prettier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import NavBar from "./components/navbar";
+import Landing from "./components/homepage/landing";
+import Resume from "./components/resumePage/resume";
+import Projects from "./components/projectsPage/projects";
+import Zworld from "./components/zworldPage/zworld";
+import Blog from "./components/blogPage/blog";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar />
+            <Routes>
+                <Route path="/home" element={<Landing />} />
+                <Route path="/resume" element={<Resume />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/zworld" element={<Zworld />} />
+                <Route path="/" element={<Navigate replace to="/home" />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/homepage/landing.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+// import HeroBanner from "./heroBanner";
+// import LocationBody from "./locationBody";
+import "./styles/landing.css";
+
+class Landing extends Component {
+    render() {
+        return (
+            <div className="landing-body">
+                {/* This div will hold all elements of the page */}
+                <p className="remove-margin">hello</p>
+                <p>lets see where this ends up rendering</p>
+            </div>
+        );
+    }
+}
+
+export default Landing;
