d38f31d6263a9233e3c820f784a45dbe792db83c
Kayeris <shaiza@uoguelph.ca>
2021-12-26 20:53:55 -0500

    all custom bgs per page done!

--- src/components/projectsPage/projects.jsx
+++ src/components/projectsPage/projects.jsx
@@ -4,7 +4,7 @@ import "./styles/projects.css";
 class Projects extends Component {
   render() {
     return (
-      <div className="landing-body">
+      <div className="projects-body">
         {/* This div will hold all elements of the page */}
         <p className="remove-margin">hello</p>
         <p className="per-page">THIS IS PROJECTSSS</p>
--- src/components/resumePage/resume.jsx
+++ src/components/resumePage/resume.jsx
@@ -4,7 +4,7 @@ import "./styles/resume.css";
 class Resume extends Component {
   render() {
     return (
-      <div className="landing-body">
+      <div className="resume-body">
         {/* This div will hold all elements of the page */}
         <p className="remove-margin">hello</p>
         <p className="per-page">THIS IS RESUMEEEEEEEEE</p>
